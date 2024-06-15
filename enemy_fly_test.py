4d7337b4f255243d4d18bfd80f6a71a251d5d702
jamad <jamad2001@gmail.com>
2024-06-15 09:18:21 +0300

    Update enemy_fly_test.py

--- enemy_fly_test.py
+++ enemy_fly_test.py
@@ -1,40 +1,37 @@
-import pyxel , math
+import pyxel, math
 
-debugdisp=0
-
+# Global Constants
 APP_WIDTH = 480
 APP_HEIGHT = 640
-CHAR_SIZE=16 
+CHAR_SIZE = 16
+
+# Global Variables
+debugdisp = 0
 
 class Squad:
     def __init__(self):
-        self.x, self.y = CHAR_SIZE*6, CHAR_SIZE*4
+        self.x, self.y = CHAR_SIZE * 6, CHAR_SIZE * 4
         self.dx = 0.2
-        self.dy = 0
-        self.list = [[],[],[],[]] # enemy arrays
+        self.list = [[], [], [], []]  # enemy arrays
 
     def update(self):
-        self.x += self.dx # horizontal group move
+        self.x += self.dx  # horizontal group move
+        theta = pyxel.frame_count / 180 * math.pi
+        self.y += math.sin(theta) * 0.25
+        if not (CHAR_SIZE <= self.x <= CHAR_SIZE * 9):
+            self.dx *= -1  # reverse direction
 
-        theta= pyxel.frame_count/180*math.pi
-        #print(theta)
-        self.y += math.sin(theta) * 0.25 # *CHAR_SIZE
-        if not (CHAR_SIZE <= self.x <= CHAR_SIZE*9): self.dx *= -1 # reverse direction
-
-        ### 移動開始させるかどうかの判定
-        attack_interval=15 # 0.25 sec each
-
+        # Determine whether to start moving
+        attack_interval = 15  # 0.25 sec each
         if pyxel.frame_count % attack_interval == 0:
+            enemy_alive = [enemy for row in self.list for enemy in row]
+            if enemy_alive:
+                chosen = enemy_alive[pyxel.rndi(0, len(enemy_alive) - 1)]  # choose at random
+                chosen.is_flying = True
+                chosen.dx = (-1, 1)[pyxel.rndi(0, 1)]
+                chosen.fly()
 
-            enemy_alive=[enemy for row in enemy_group.list for enemy in row]
-            chosen=enemy_alive[pyxel.rndi(0,len(enemy_alive)-1)] # choose at random
-            chosen.is_flying = True
-            chosen.dx= (-1,1)[pyxel.rndi(0,1)]
-            chosen.fly()
-
-enemy_group = Squad() 
-
-import math
+enemy_group = Squad()
 
 class Teki:
     def __init__(self, rx, ry, num):
@@ -66,32 +63,27 @@ class Teki:
 
     def update(self):
         self.cnt += 1
-
         if self.is_return:
             tx = enemy_group.x + self.rposx
             ty = enemy_group.y + self.rposy
             if self.move(tx, ty):
                 self.is_flying = False
                 self.is_return = False
-
         elif self.is_flying:
-            if len(self.trajectory) > 0:
+            if self.trajectory:
                 tx, ty = self.trajectory[0]
                 if self.move(tx, ty):
                     self.trajectory.pop(0)
-
-                # Teleport enemy to top of the screen if out of screen
                 if APP_HEIGHT + 32 < self.y:
                     self.is_return = True
                     self.y = -CHAR_SIZE * 2
                     self.x = APP_WIDTH / 2
-
         else:
             self.x = enemy_group.x + self.rposx
             self.y = enemy_group.y + self.rposy
 
     def draw(self):
-        u = self.is_flying and 2 + (0 < self.dx) or (self.cnt // 30) % 2
+        u = 2 + (0 < self.dx) if self.is_flying else (self.cnt // 30) % 2
         v = self.num + 3
         pyxel.blt(self.x, self.y, 0, u * 16, v * 16, 16, 16, 0)
 
@@ -102,16 +94,12 @@ class Teki:
 
     def fly(self):
         self.is_flying = True
-        px = self.x +50
-        py = self.y +1000
-
-        # Generate smooth trajectory through given points
+        px = self.x + 50
+        py = self.y + 1000
         self.trajectory = self.generate_smooth_trajectory(px, py)
-
-        self.dy = -1  # Example direction adjustment
+        self.dy = -1
 
     def generate_smooth_trajectory(self, px, py):
-        # Generate smooth trajectory passing through given points
         trajectory = []
         num_points = 10  # Number of points for interpolation
         for i in range(num_points):
@@ -121,57 +109,49 @@ class Teki:
             trajectory.append([tx, ty])
         return trajectory
 
-
-    def check_hit(self,shipx,shipy) :
+    def check_hit(self, shipx, shipy):
         return abs(shipx - self.x) < 12 and abs(shipy - self.y) < 12
 
-
-
 class App:
-    # moved global variable here by using App.variablename 
     score = 0
-    stage_number=0 
-    message_list = []# moved the variable here instead of global variable
+    stage_number = 0
+    message_list = []
     bullet_list = []
     tekibullets = []
-    
+
     def __init__(self):
-        pyxel.init(APP_WIDTH,APP_HEIGHT,title="Kanixian MOD",fps=120,display_scale=1) 
+        pyxel.init(APP_WIDTH, APP_HEIGHT, title="Kanixian MOD", fps=120, display_scale=1)
         pyxel.load("kani.pyxres")
-
-        pyxel.run(self.update,self.draw)
-
+        pyxel.run(self.update, self.draw)
 
     def init_stage(self):
-        App.bullet_list = []# need to empty otherwise, instant death could happen
-        App.tekibullets = []# need to empty otherwise, instant death could happen
+        App.bullet_list = []
+        App.tekibullets = []
         App.stage_number += 1
-        self.counter = 0
-        MAX_COL_NUM=16*2
-        enemy_group.list = [[Teki(x*10,i*20,i)for x in R] for i, R in enumerate( [(4+3*2,10+5*2),range(2,MAX_COL_NUM-2,2)]+[range(0,MAX_COL_NUM,2)]*5 )]
+        MAX_COL_NUM = 16 * 2
+        enemy_group.list = [[Teki(x * 10, i * 20, i) for x in R] for i, R in enumerate([(4 + 3 * 2, 10 + 5 * 2), range(2, MAX_COL_NUM - 2, 2)] + [range(0, MAX_COL_NUM, 2)] * 5)]
 
     def update(self):
-
-        ### ステージクリアの判定
-        if sum(map(len,enemy_group.list))==0:
+        if sum(map(len, enemy_group.list)) == 0:
             self.init_stage()
             return
 
+        App.bullet_list = [bullet for bullet in App.bullet_list if bullet.y >= -10]
+        App.tekibullets = [bullet for bullet in App.tekibullets if bullet.y <= APP_HEIGHT + 10]
 
-        ### 弾の生存確認
-        [App.bullet_list.remove(bullet)for bullet in App.bullet_list if bullet.y < -10]
-        [App.tekibullets.remove(bullet) for bullet in App.tekibullets if bullet.y > APP_HEIGHT + 10]
-
-        [bullet.update() for bullet in App.bullet_list+App.tekibullets]             # 弾の更新
-        enemy_group.update()                                                # 分隊の更新
-        [teki.update() for tekis in enemy_group.list for teki in tekis]     # 敵の更新    
+        for bullet in App.bullet_list + App.tekibullets:
+            bullet.update()
+        enemy_group.update()
+        for tekis in enemy_group.list:
+            for teki in tekis:
+                teki.update()
 
     def draw(self):
         pyxel.cls(0)
-
-        # core contents
-        if 1:            
-            [teki.draw() for tekis in enemy_group.list for teki in tekis]   # 敵の描画
-            [bullet.draw() for bullet in App.bullet_list+App.tekibullets]           # 弾の描画
+        for tekis in enemy_group.list:
+            for teki in tekis:
+                teki.draw()
+        for bullet in App.bullet_list + App.tekibullets:
+            bullet.draw()
 
 App()
