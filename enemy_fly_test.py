f132a406f7673bd8261f16c758beca1dd8504f83
jamad <jamad2001@gmail.com>
2024-06-15 10:08:07 +0300

    Update enemy_fly_test.py

--- enemy_fly_test.py
+++ enemy_fly_test.py
@@ -2,24 +2,24 @@ import pyxel, math
 
 APP_WIDTH, APP_HEIGHT = 480, 640
 CHAR_SIZE = 16
+
 class Squad:
     def __init__(self):
         self.x, self.y = CHAR_SIZE * 6, CHAR_SIZE * 4
-        self.dx = 0.2
-        self.list = [[], [], [], []]
+        self.dx = 0.2                                                   # horizontal move speed
+        self.list = []
 
     def update(self):
-        self.x += self.dx
-        self.y += math.sin(pyxel.frame_count / 180 * math.pi) * 0.25
-        if not (CHAR_SIZE <= self.x <= CHAR_SIZE * 9):
-            self.dx *= -1
-        if pyxel.frame_count % 15 == 0:
-            enemy_alive = [enemy for row in self.list for enemy in row if enemy]
-            if enemy_alive:
-                chosen = enemy_alive[pyxel.rndi(0, len(enemy_alive) - 1)]
-                chosen.is_flying = True
-                chosen.dx = (-1, 1)[pyxel.rndi(0, 1)]
-                chosen.fly()
+        self.x += self.dx                                                       # horizontal move
+        if not (CHAR_SIZE <= self.x <= CHAR_SIZE * 9):self.dx *= -1             # horizontal direction change
+        self.y += math.sin(pyxel.frame_count / 180 * math.pi) * 0.25            # vertical move
+
+        if pyxel.frame_count % 15 == 0:                                         # flying phase
+            enemies = [enemy for row in self.list for enemy in row if enemy]    
+            chosen = enemies[pyxel.rndi(0, len(enemies) - 1)]
+            chosen.is_flying = True
+            chosen.dx = (-1, 1)[pyxel.rndi(0, 1)]                               # random direction
+            chosen.fly()
 
 enemy_group = Squad()
 
@@ -27,7 +27,7 @@ class Teki:
     def __init__(self, rx, ry, num):
         self.rposx, self.rposy = rx, ry
         self.num = min(3, num)
-        self.cnt, self.is_flying, self.is_return = 0, False, False
+        self.cnt = self.is_flying = self.is_return = 0
         self.x, self.y = enemy_group.x + self.rposx, enemy_group.y + self.rposy
         self.dx, self.dy, self.trajectory = 0, 0, []
 
@@ -54,7 +54,7 @@ class Teki:
 
     def draw(self):
         u = 2 + (0 < self.dx) if self.is_flying else (self.cnt // 30) % 2
-        pyxel.blt(self.x, self.y, 0, u * 16, (self.num + 3) * 16, 16, 16, 0)
+        pyxel.blt(self.x, self.y, 0, u * CHAR_SIZE, (self.num + 3) * CHAR_SIZE, CHAR_SIZE, CHAR_SIZE, 0)
 
     def fly(self):
         self.is_flying = True
@@ -66,8 +66,6 @@ class Teki:
         self.dy = -1
 
 class App:
-    stage_number = 0
-
     def __init__(self):
         pyxel.init(APP_WIDTH, APP_HEIGHT, title="Kanixian MOD", fps=120)
         pyxel.load("kani.pyxres")
